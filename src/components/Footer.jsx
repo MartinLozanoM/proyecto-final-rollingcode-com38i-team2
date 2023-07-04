748989183262c17b8949cb63eb6a1a41edfaa4b9
Martin Lozano Muñoz <martinlozanomu@gmail.com>
2023-07-03 21:44:39 -0300

    add react-bootstrap, react-router-dom, react-icons, some components (navbar, home, and footer)

---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export const Footer = () => {
+  return (
+    <footer>
+      <div>Hola soy el footer</div>
+    </footer>
+  );
+};
---
+++ src/components/Home.jsx
@@ -0,0 +1,7 @@
+export const Home = () => {
+  return (
+    <main>
+      <div>Hola soy la home</div>
+    </main>
+  );
+};
