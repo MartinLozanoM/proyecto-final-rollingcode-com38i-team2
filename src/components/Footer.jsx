4ceb428b40748a4f73cd7a5fa01969b2c990ccca
Martin Lozano Muñoz <martinlozanomu@gmail.com>
2023-07-13 22:12:53 -0300

    create utils folder and footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,35 @@
+import Logo from "../assets/react.svg";
+import { FaFacebookSquare } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaTwitterSquare } from "react-icons/fa";
+
 export const Footer = () => {
   return (
     <footer>
-      <div>Hola soy el footer</div>
+      <div className="footer-container">
+        <div className="col-md-6">
+          <img src={Logo} alt="Logo" width="100" height="60" />
+        </div>
+        <div className="col-md-6">
+          <div>Seguinos:</div>
+          <div>
+            <a href="https://es-la.facebook.com/">
+              <FaFacebookSquare />
+            </a>
+            <a href="https://www.instagram.com/">
+              <FaInstagram />
+            </a>
+            <a href="https://www.linkedin.com/">
+              <FaTwitterSquare />
+            </a>
+          </div>
+        </div>
+        <div className="col-md-12">
+          <span>
+            Copyright © 2023 / Rolling Code. Todos los derechos reservados.
+          </span>
+        </div>
+      </div>
     </footer>
   );
 };
