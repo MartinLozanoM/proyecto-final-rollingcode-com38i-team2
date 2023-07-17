d5d700d2f14cddfa184a3c34ae030ae5534605dd
Martin Lozano Muñoz <martinlozanomu@gmail.com>
2023-07-17 17:19:34 -0300

    repair error in deploy

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,35 +1,31 @@
 import React, { useState, useEffect } from "react";
-import "./Slider.css";
-import Gato from "../assets/images/gato.jpg"
-import Pajaro from "../assets/images/pajaro.jpg"
-import Perro from "../assets/images/perro.jpg"
+import "./slider.css";
+import Gato from "../assets/images/gato.jpg";
+import Pajaro from "../assets/images/pajaro.jpg";
+import Perro from "../assets/images/perro.jpg";
 
-const images = [
-  Gato,
-  Pajaro,  
-  Perro
-];
+const images = [Gato, Pajaro, Perro];
 
 function Slider() {
   const [activeSlide, setActiveSlide] = useState(0);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setActiveSlide(prevActiveSlide => {
+      setActiveSlide((prevActiveSlide) => {
         if (prevActiveSlide === images.length - 1) {
           return 0;
-        } else {    
+        } else {
           return prevActiveSlide + 1;
-        }    
+        }
       });
-     }, 2000);   
-    return () => clearInterval(interval);  
+    }, 2000);
+    return () => clearInterval(interval);
   }, [activeSlide]);
 
   return (
     <div className="slider">
       <img src={images[activeSlide]} alt="" />
-    </div> 
+    </div>
   );
 }
 
