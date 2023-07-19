6eb17a4a913203962740be38a55155bff34f9555
Andrea Toledo <atoledo618@gmail.com>
2023-07-19 17:54:04 -0300

    :sparkles: add navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,15 @@
+import Logo from "../assets/images/logo-developets.png";
+
 export const Navbar = () => {
   return (
-    <nav>
-      <div>Hola soy el navbar</div>
+    <nav className="navbar">
+      <a href="#"><img src={Logo} alt="Logo"/></a>
+      <div className="nav_items">
+        <a href="#"> Inicio</a>
+        <a href="#"> Sobre Nosotros</a>
+        <a href="#"> Servicios</a>
+        <a href="#"> Contacto</a>
+      </div>
     </nav>
   );
 };
