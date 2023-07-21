fd3b6f65060497e1f1717e6f40c7f7c9db5cceaa
Andrea Toledo <atoledo618@gmail.com>
2023-07-20 22:14:52 -0300

    :sparkles: navbar change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,35 @@
-import Logo from "../assets/images/logo-developets.png";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import { Link } from 'react-router-dom';
 
-export const Navbar = () => {
+
+export const NavbarPractice = () => {
   return (
-    <nav className="navbar">
-      <a href="#"><img src={Logo} alt="Logo"/></a>
-      <div className="nav_items">
-        <a href="#"> Inicio</a>
-        <a href="#"> Sobre Nosotros</a>
-        <a href="#"> Servicios</a>
-        <a href="#"> Contacto</a>
-      </div>
-    </nav>
+    <Navbar expand="lg" className="navbar">
+      <Container className='nav_items'>
+        <Navbar.Brand href="#home">LOGO</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+           <Link to="/">Inicio</Link>
+           <Link to="/products ">Productos</Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+
   );
 };
