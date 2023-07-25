b3757b0f96896761f0feef44215794d9eaddda65
Martin Lozano Muñoz <martinlozanomu@gmail.com>
2023-07-24 23:26:09 -0300

    change the color of the navbar, add logo, and put media queries

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,28 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import { Link } from 'react-router-dom';
-
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { Link } from "react-router-dom";
+import Logo from "../assets/images/logo-developets.png";
 
 export const NavbarPractice = () => {
   return (
     <Navbar expand="lg" className="navbar">
-      <Container className='nav_items'>
-        <Navbar.Brand><Link to="/">LOGO</Link></Navbar.Brand>
+      <Container className="nav_items">
+        <Navbar className="navbar-container-logo">
+          <Link to="/">
+            <img className="navbar-logo" src={Logo} alt="Logo" />
+          </Link>
+        </Navbar>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-           <Link to="/">Inicio</Link>
-           <Link to="/products ">Productos</Link>
-           <Link to="/">Sobre Nosotros</Link>
-           <Link to="/products">Mi cuenta</Link>
+        <Navbar.Collapse className="navbar-container" id="basic-navbar-nav">
+          <Nav>
+            <Link to="/">Inicio</Link>
+            <Link to="/products ">Productos</Link>
+            <Link to="/">Sobre Nosotros</Link>
+            <Link to="/products">Mi cuenta</Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
-
   );
 };
